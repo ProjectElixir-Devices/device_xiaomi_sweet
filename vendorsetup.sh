f249ff48ebbbea65ea8b6acb6e2efdf9b946eb7a
pure-soul-kk <krishnakripa34567@gmail.com>
2023-11-19 14:04:54 +0530

    sweet: Add vendorsetup.sh & remove.txt for CLI Based Builds
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+# Common Device Tree
+rm -rf device/xiaomi/sm6150-common
+git clone -b UNO https://github.com/ProjectElixir-Devices/device_xiaomi_sm6150-common_sweet device/xiaomi/sm6150-common
+
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6150-common-sweet vendor/xiaomi/sm6150-common
+
+# Miui Camera
+git clone --depth=1 https://gitlab.com/unmoved21/sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone --depth=1 https://github.com/pure-soul-kk/kernel_xiaomi_sweet kernel/xiaomi/sm6150
+
+# Aosp 17.0.4 Clang
+rm -rf prebuilts/clang/host/linux-x86/r498229b
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b

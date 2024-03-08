10d96e91ee4ddefa8f43e84c53417d9f5d29e0f4
K R I S H N A <krishnakripa34567@gmail.com>
2024-03-08 12:40:21 +0000

    sweet: Build NFC Packages
    *NfcNci and SecureElement

--- device.mk
+++ device.mk
@@ -46,7 +46,9 @@ PRODUCT_COPY_FILES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    Tag
+    Tag \
+    NfcNci \
+    SecureElement
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
